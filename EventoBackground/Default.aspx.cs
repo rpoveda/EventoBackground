1a49a9eca481cfcd6a27704d2df4d6cb2121d6b2
Rafael Poveda <raffael.poveda@gmail.com>
2012-05-06 21:27:54 -0300

    files

---
+++ EventoBackground/Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EventoBackground
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnExecuta_OnClick(object sender, EventArgs e)
+        {
+            MyThread.GravaArquivo();
+            Response.Redirect("Redirect.aspx");
+        }
+    }
+}
---
+++ EventoBackground/MyThread.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Web;
+
+namespace EventoBackground
+{
+    public static class MyThread
+    {
+        private static AutoResetEvent objAutoResetEvent;
+        private static string path;
+        public static void GravaArquivo()
+        {
+            objAutoResetEvent = new AutoResetEvent(true);
+            ThreadPool.QueueUserWorkItem(Executa);
+        }
+        private static void Executa(object state)
+        {
+            path = @"c:\temp\";
+            var file = path + DateTime.Now.ToString("ddMMyyyHHmmss") + ".txt";
+            using (StreamWriter writer = new StreamWriter(file))
+            {
+                writer.WriteLine("Ola mundo");
+                writer.WriteLine("Escrevendo um txto em BackGround");
+            }
+            objAutoResetEvent.Set();
+        }
+    }
+}
